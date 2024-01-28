0929bb5da6fb0dc4f417561d15192c6360de2065
tiago1820 <tiago.zdo@gmail.com>
2024-01-28 13:07:13 -0300

    A helper was created to format the characters before sending them to the frontend.

--- src/services/character.service.js
+++ src/services/character.service.js
@@ -1,23 +1,17 @@
 const { Character } = require("../db.js");
+const FormattedData = require("../helpers/formattedData.helper.js");
 
 class CharacterService {
+    constructor() {
+        this.format = new FormattedData();
+    }
 
     getAllCharacters = async () => {
         try {
             const characters = await Character.findAll();
 
-            const formattedData = {
-                info: {
-                    count: characters.length,
-                    pages: 1,
-                    next: null,
-                    prev: null
-                },
-                results: characters
-            };
-
-            return formattedData;
-            
+            return this.format.formattedCharacter(characters);
+    
         } catch (error) {
             console.log("ERROR: ", error);
             throw error;
