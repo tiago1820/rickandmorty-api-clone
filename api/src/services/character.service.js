83166d973d75dedbdb86747e329e262424d692e0
tiago1820 <tiago.zdo@gmail.com>
2024-02-16 21:32:53 -0300

    Now the location table is updated in its residents field every time a new character is created.

--- api/src/services/character.service.js
+++ api/src/services/character.service.js
@@ -1,4 +1,4 @@
-const { Character } = require("../db.js");
+const { Character, Location } = require("../db.js");
 
 class CharacterService {
 
@@ -22,17 +22,28 @@ class CharacterService {
             data.created = new Date().toISOString();
             try {
                 let createdCharacter = await Character.create(data);
-                const { id } = createdCharacter;
-                const characterUrl = "http://localhost:3001/character/" + id;
+                const { id: charId } = createdCharacter;
+                const characterUrl = "http://localhost:3001/character/" + charId;
 
                 await Character.update({ url: characterUrl }, {
-                    where: { id: id }
+                    where: { id: charId }
                 });
 
-                createdCharacter = await Character.findByPk(id);
-                const { dataValues } = createdCharacter;
-                
-                return dataValues;
+                createdCharacter = await Character.findByPk(charId);
+                let { dataValues } = createdCharacter;
+
+                const [prueba] = await Location.findAll({ where: { id: dataValues.location.id } })
+
+                const { residents } = prueba.dataValues;
+                residents.push(characterUrl);
+
+                await Location.update({ residents: residents }, {
+                    where: { id: prueba.id }
+                })
+
+                createdCharacter = await Character.findByPk(charId);
+
+                return createdCharacter.dataValues;
             } catch (error) {
                 throw error;
             }
